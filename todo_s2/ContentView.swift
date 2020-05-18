e34935a56cae592b340d66961122ee7345e56d26
kuroyanagi66 <56671239+kuroyanagi66@users.noreply.github.com>
2020-05-18 23:07:25 +0900

    Initial Commit

---
+++ todo_s2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo_s2
+//
+//  Created by Soichiro Kuroyanagi on 2020/05/18.
+//  Copyright © 2020 kute. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
